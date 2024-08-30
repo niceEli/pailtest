ed6cbd2f711f32d7d2523e04bff5ba29f8e90e9d
Eli <78963198+niceEli@users.noreply.github.com>
2024-08-29 20:13:09 -0400

    feat: make better

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
 import { $ } from "bun";
 let pkgs = await $`scoop list`.quiet();
-console.log('Hello, Pail!\n');
 
-console.log(`${pkgs.stdout}`);
+let stdout = pkgs.stdout.toString();
+
+stdout = stdout.slice(285);
+
+const rows = stdout.split('\n');
+
+const firstColumn = rows.map(row => {
+  const columns = row.split(' ');
+  return columns[0];
+});
+
+const col = firstColumn.join(' ');
+
+console.log(`Hello, Pail!\n\nYour Scoop Packages Are:  ${col}`);
